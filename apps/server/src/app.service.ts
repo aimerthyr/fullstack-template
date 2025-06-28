1d38f45f8d511cdb3ee58211a7dcdc1230ae71b7
aimerthyr <aimerthy@foxmail.com>
2025-06-28 16:40:11 +0800

    chore: initialize full-stack monorepo

---
+++ apps/server/src/app.service.ts
@@ -0,0 +1,7 @@
+import { NAME } from "@internal/constants";
+import { Injectable } from "@nestjs/common";
+
+@Injectable()
+export class AppService {
+  getHello = (): string => `The meaning of life is: ${NAME}`;
+}
