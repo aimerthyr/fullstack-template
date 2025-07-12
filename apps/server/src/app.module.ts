1015293af62f9ea67cdf8fa07f89795ac06cc398
aimerthyr <aimerthy@foxmail.com>
2025-07-12 17:30:57 +0800

    feat: support typeorm

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
@@ -10,6 +11,16 @@ import { AppService } from './app.service';
       expandVariables: true,
       cache: true,
       envFilePath: `.env.${process.env.NODE_ENV || 'development'}`,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      autoLoadEntities: true,
+      synchronize: process.env.NODE_ENV !== 'production',
     }),
   ],
   controllers: [AppController],
